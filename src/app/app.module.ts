36f216c875f4a123672d92a8116af253c4358f8f
Vuong Nguyen <yeungon@gmail.com>
2020-06-12 10:42:10 +0700

    Add the routing and completing the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,    
-    TodosComponent, TodoItemComponent, HeaderComponent, AddTodoComponent
+    TodosComponent, TodoItemComponent, HeaderComponent, AddTodoComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
